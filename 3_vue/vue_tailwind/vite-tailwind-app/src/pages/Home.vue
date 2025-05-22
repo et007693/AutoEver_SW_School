c1f806f8c3cd4f2cfac05782de8daecfbdd32517
송창용 <thdckdyd123@naver.com>
2025-05-22 17:50:12 +0900

    fix: 버그 수정

--- 3_vue/vue_tailwind/vite-tailwind-app/src/pages/Home.vue
+++ 3_vue/vue_tailwind/vite-tailwind-app/src/pages/Home.vue
@@ -1,46 +1,50 @@
 <template>
-  <div class="user-info">
-    <p>이름 : {{ userInfo.name }}</p>
-    <p>이메일 : {{ userInfo.email }}</p>
-  </div>
-  <div class="min-h-screen bg-transparent flex items-center justify-center p-4">
-    <div class="grid grid-cols-2 gap-4 w-full max-w-4xl">
-      <button
-        class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
-        @click="navigateTo('/members')"
-      >
-        회원리스트
-      </button>
-      <button
-        class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
-        @click="navigateTo('/boards')"
-      >
-        게시판
-      </button>
-      <button
-        class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
-        @click="navigateTo('/category')"
-      >
-        카테고리
-      </button>
-      <button
-        class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
-        @click="navigateTo('/theme')"
-      >
-        테마 설정
-      </button>
-      <button
-        class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
-        @click="navigateTo('/category')"
-      >
-        카테고리 설정
-      </button>
-      <button
-        class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
-        @click="navigateTo('/Chat')"
-      >
-        채팅
-      </button>
+  <div>
+    <div class="user-info">
+      <p>이름 : {{ userInfo.name }}</p>
+      <p>이메일 : {{ userInfo.email }}</p>
+    </div>
+    <div
+      class="min-h-screen bg-transparent flex items-center justify-center p-4"
+    >
+      <div class="grid grid-cols-2 gap-4 w-full max-w-4xl">
+        <button
+          class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
+          @click="navigateTo('/members')"
+        >
+          회원리스트
+        </button>
+        <button
+          class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
+          @click="navigateTo('/boards')"
+        >
+          게시판
+        </button>
+        <button
+          class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
+          @click="navigateTo('/category')"
+        >
+          카테고리
+        </button>
+        <button
+          class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
+          @click="navigateTo('/theme')"
+        >
+          테마 설정
+        </button>
+        <button
+          class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
+          @click="navigateTo('/category')"
+        >
+          카테고리 설정
+        </button>
+        <button
+          class="bg-transparent border border-white text-white font-semibold py-6 rounded-lg"
+          @click="navigateTo('/chat')"
+        >
+          채팅
+        </button>
+      </div>
     </div>
   </div>
 </template>
